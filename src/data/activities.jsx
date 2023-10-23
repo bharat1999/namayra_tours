c609f2c332a1b97d740d635d9b36a7f80e82bc9c
Bharat Malik <bharat.malik@roro.io>
2023-10-24 02:10:23 +0530

    image route fix

---
+++ src/data/activities.jsx
@@ -0,0 +1,146 @@
+import burj_al_arab_1 from "../assets/small_images/Burj Al Arab Tour 1.webp";
+import burj_al_arab_2 from "../assets/small_images/Burj Al Arab Tour 2.webp";
+import burj_al_arab_3 from "../assets/small_images/Burj Al Arab Tour 3.webp";
+import burj_khalifa_1 from "../assets/small_images/Burj Khalifa 1.webp";
+import burj_khalifa_2 from "../assets/small_images/Burj Khalifa 2.webp";
+import burj_khalifa_3 from "../assets/small_images/Burj Khalifa 4.webp";
+import desert_safari_1 from "../assets/small_images/Desert Safari 1.webp";
+import desert_safari_2 from "../assets/small_images/Desert Safari 2.webp";
+import dubai_aquarium_1 from "../assets/small_images/Dubai Aquarium And Underwater Zoo 1.webp";
+import dubai_aquarium_2 from "../assets/small_images/Dubai Aquarium And Underwater Zoo 2.webp";
+import dubai_aquarium_3 from "../assets/small_images/Dubai Aquarium And Underwater Zoo 3.webp";
+import dubai_dophinarium_1 from "../assets/small_images/Dubai Dolphinarium 1.webp";
+import dubai_dophinarium_2 from "../assets/small_images/Dubai Dolphinarium 2.webp";
+import dubai_dophinarium_3 from "../assets/small_images/Dubai Dolphinarium 3.webp";
+import dubai_frame_1 from "../assets/small_images/Dubai Frame 1.webp";
+import dubai_frame_2 from "../assets/small_images/Dubai Frame 2.webp";
+import dubai_frame_3 from "../assets/small_images/Dubai Frame 3.webp";
+import dubai_frame_4 from "../assets/small_images/Dubai Frame 4.webp";
+import hot_air_ballon_1 from "../assets/small_images/Hot Air Balloon 1.webp";
+import hot_air_ballon_2 from "../assets/small_images/Hot Air Balloon 2.webp";
+import miracle_garden_1 from "../assets/small_images/Miracle Garden Dubai 1.webp";
+import miracle_garden_2 from "../assets/small_images/Miracle Garden Dubai 2.webp";
+import miracle_garden_3 from "../assets/small_images/Miracle Garden Dubai 3.webp";
+import miracle_garden_4 from "../assets/small_images/Miracle Garden Dubai 4.webp";
+import future_museum_1 from "../assets/small_images/Museum Of The Future 1.webp";
+import future_museum_2 from "../assets/small_images/Museum Of The Future 2.webp";
+import future_museum_3 from "../assets/small_images/Museum Of The Future 3.webp";
+import skydiving_1 from "../assets/small_images/Skydiving 1.webp";
+import skydiving_2 from "../assets/small_images/Skydiving 2.webp";
+
+const activityCategories = {
+  Dubai: [
+    {
+      heading: "Most Popular Experiences in Dubai",
+      subheading:
+        "Culture, nature, thrills, and record-breaking experiences—Dubai is the place to seek out everything you imagine and beyond. Find it all here!",
+      activities: [
+        {
+          key: "1",
+          title: "Burj Al Arab Tour",
+          images: [burj_al_arab_1, burj_al_arab_2, burj_al_arab_3],
+        },
+        {
+          key: "2",
+          title: "Burj Khalifa",
+          images: [burj_khalifa_1, burj_khalifa_2, burj_khalifa_3],
+        },
+        {
+          key: "3",
+          title: "Desert Safari",
+          images: [desert_safari_1, desert_safari_2],
+        },
+        {
+          key: "4",
+          title: "Dubai Aquarium & Underwater Zoo",
+          images: [dubai_aquarium_1, dubai_aquarium_2, dubai_aquarium_3],
+        },
+        {
+          key: "5",
+          title: "Dubai Dolphinarium",
+          images: [
+            dubai_dophinarium_1,
+            dubai_dophinarium_2,
+            dubai_dophinarium_3,
+          ],
+        },
+        {
+          key: "6",
+          title: "Dubai Frame",
+          images: [dubai_frame_1, dubai_frame_2, dubai_frame_3, dubai_frame_4],
+        },
+        {
+          key: "7",
+          title: "Hot Air Ballon",
+          images: [hot_air_ballon_1, hot_air_ballon_2],
+        },
+        {
+          key: "8",
+          title: "Miracle Garden Dubai",
+          images: [
+            miracle_garden_1,
+            miracle_garden_2,
+            miracle_garden_3,
+            miracle_garden_4,
+          ],
+        },
+        {
+          key: "9",
+          title: "Museum Of The Future",
+          images: [future_museum_1, future_museum_2, future_museum_3],
+        },
+        {
+          key: "10",
+          title: "Skydiving",
+          images: [skydiving_1, skydiving_2],
+        },
+      ],
+    },
+  ],
+  // AbuDhabi: [
+  //   {
+  //     heading: "Top Experiences in Abu Dhabi",
+  //     subheading:
+  //       "Experience the thrill of discovering off-the-beaten-path gems and famous landmarks in top destinations worldwide - your wanderlust awaits!",
+  //     activities: [
+  //       {
+  //         key: "11",
+  //         title: "Half Day Dubai City Tour in Private Car",
+  //         duration: "4 Hours",
+  //         image: "https://picsum.photos/80",
+  //         price: "249",
+  //       },
+  //       {
+  //         key: "12",
+  //         title: "Dubai Full Day City Tour in Private Car",
+  //         duration: "9 Hours",
+  //         image: "https://picsum.photos/80",
+  //         price: "249",
+  //       },
+  //       {
+  //         key: "13",
+  //         title: "Half Day Dubai City Tour in Private Car",
+  //         duration: "4 Hours",
+  //         image: "https://picsum.photos/80",
+  //         price: "249",
+  //       },
+  //       {
+  //         key: "14",
+  //         title: "Dubai Full Day City Tour in Private Car",
+  //         duration: "9 Hours",
+  //         image: "https://picsum.photos/80",
+  //         price: "249",
+  //       },
+  //       {
+  //         key: "15",
+  //         title: "Dubai Full Day City Tour in Private Car",
+  //         duration: "9 Hours",
+  //         image: "https://picsum.photos/80",
+  //         price: "249",
+  //       },
+  //     ],
+  //   },
+  // ],
+};
+
+export default activityCategories;
