d98d94c5a7a5098c5bafda18533ed319603f2f28
Bharat Malik <bharat.malik@roro.io>
2023-11-07 23:32:59 +0530

    video poster added

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,9 +1,10 @@
 import { Container } from "react-bootstrap";
 import video from "../assets/video/video.mp4";
+import poster from "../assets/video/poster.png";
 export default function VideoPlayer() {
   return (
     <Container className="mt-5 p-5 video-container">
-      <video controls>
+      <video poster={poster} controls>
         <source src={video} type="video/mp4" />
       </video>
     </Container>
