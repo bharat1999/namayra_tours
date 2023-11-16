e9dbed50de81253dfb1651dea47c5de9e1e95cd6
Bharat Malik <bharat.malik@roro.io>
2023-11-16 20:47:00 +0530

    navbar updated

--- src/components/CustomNavbar.jsx
+++ src/components/CustomNavbar.jsx
@@ -1,15 +1,36 @@
+import { useEffect, useState } from "react";
 import { NavDropdown } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 export default function CustomNavbar() {
+  const [path, setPath] = useState("");
+
+  useEffect(() => {
+    let currentPath = window.location.pathname;
+    currentPath = currentPath.substring(1);
+    setPath(currentPath);
+  }, []);
   return (
     <Navbar expand="lg" data-bs-theme="light">
       <Container className="justify-content-center">
         <Nav>
           <Nav.Item className="text-center">
-            <Nav.Link href="/about" className="text-white">
+            <Nav.Link
+              href="/"
+              className="text-white"
+              active={path == "" ? true : false}
+            >
+              Home
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item className="text-center">
+            <Nav.Link
+              href="/about"
+              className="text-white"
+              active={path == "about" ? true : false}
+            >
               About Us
             </Nav.Link>
           </Nav.Item>
